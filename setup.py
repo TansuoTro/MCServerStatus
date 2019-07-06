37c2625b8dd678933d563a903aece61dca52bb92
晚餐骨同志 <40848544+CarierxGroup@users.noreply.github.com>
2019-07-07 07:51:30 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import sys
+PY2 = sys.version_info[0] == 2
+
+install_requires = [
+    'six', 'Click'
+]
+
+if PY2:
+    install_requires.append('dnspython')
+else:
+    install_requires.append('dnspython3')
+
+tests_require = [
+    'mock'
+]
+
+setup(
+    name='mcstatus',
+    version='2.2.1',
+    author='Nathan Adams',
+    author_email='dinnerbone@dinnerbone.com',
+    url='https://pypi.python.org/pypi/mcstatus',
+    packages=['mcstatus', 'mcstatus.protocol', 'mcstatus.scripts'],
+    description='A library to query Minecraft Servers for their status and capabilities.',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points='''
+        [console_scripts]
+        mcstatus=mcstatus.scripts.mcstatus:cli
+    ''',
+)
